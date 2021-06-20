6aaf3207be6e302c5b94d18a97eb73c4d3885547
Cyrano webdev <cyrano@cyranowebdev.com>
2021-06-20 19:54:38 -0300

    feat(backend/teacaher): get classes with mongo aggregation

--- back-end/src/models/teachers.models.js
+++ back-end/src/models/teachers.models.js
@@ -1,8 +1,68 @@
+/* eslint-disable camelcase */
 /* eslint-disable max-lines-per-function */
 const connection = require('./connection');
 
-const getClasses = async (userId) => connection()
-  .then((db) => db.collection('classes').find({ teachers: userId }).toArray());
+const getClasses = async (userId) => {
+  const results = await connection()
+    .then((db) => db.collection('classes').aggregate([
+      { $match: { teachers: userId } },
+      {
+        $addFields: {
+          teachers: {
+            $map: {
+              input: '$teachers',
+              in: { $toObjectId: '$$this' },
+            },
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'teachers',
+          foreignField: '_id',
+          as: 'teachers',
+        },
+      },
+      { $unwind: '$comments' },
+      {
+        $lookup: {
+        from: 'users',
+        let: { teacher_id: { $toObjectId: '$comments.teacher' } },
+        pipeline: [
+           { $match: { $expr: { $eq: ['$_id', '$$teacher_id'] } } },
+         ],
+         as: 'comments.teacher',
+        },
+      },
+      {
+        $group: {
+        _id: '$_id',
+        schoolId: { $first: '$schoolId' },
+        year: { $first: '$year' },
+        class: { $first: '$class' },
+        grade: { $first: '$grade' },
+        teachers: { $first: '$teachers' },
+        comments: { $push: '$comments' },
+        students: { $first: '$students' },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          schoolId: 1,
+          year: 1,
+          class: 1,
+          grade: 1,
+          teachers: 1,
+          students: 1,
+          comments: 1,
+        },
+      },
+    ]).toArray());
+
+  return results;
+};
 
 module.exports = {
   getClasses,
