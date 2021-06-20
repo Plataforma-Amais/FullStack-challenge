312c78296d544a493eedd7b2bd5619b1b225369f
Cyrano webdev <cyrano@cyranowebdev.com>
2021-06-19 22:30:44 -0300

    fix(backend/notFound): fix route to show custom error

--- back-end/src/routes/notFound.routes.js
+++ back-end/src/routes/notFound.routes.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const { StatusCodes } = require('http-status-codes');
 
 const notFound = express.Router();
 
-const errorNotFound = {
-  statusCode: 404,
-  customCode: 'C_ERR_NOT_FOUND',
-  message: 'Not Found',
-};
+const routeNotFound = 'C_ERR_NOT_FOUND';
 
-notFound.use('*', (_req, res) => res.status(StatusCodes.NOT_FOUND).json(errorNotFound));
+notFound.use('*', (_req, _res, next) => {
+  try {
+    throw new Error(routeNotFound);
+  } catch (err) {
+    next({ err });
+  }
+});
 
 module.exports = notFound;
